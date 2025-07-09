105d8d50a13d4d5f11331041439dd2e4a6c11175
ShohbozKurbonbekov <skurbonbekov98@gmail.com>
2025-07-10 00:28:53 +0900

    feat: meta tags integration on private _document file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log("_App component - App Router");
   return <Component {...pageProps} />;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,32 @@ export default function Document() {
   console.log("DOCUMENT PAGES  ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        {/* 
+        index: Tells them to include this page in search engine results.
+
+follow: Tells them to follow the links on this page and crawl those too.
+
+it means:
+"Search engines, you are allowed to index this page and follow any links you find on it."
+         */}
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+        <meta
+          name="keyword"
+          content={"nestar, nestar.uz, devex mern mern nestjs fullstack"}
+        />
+        <meta
+          name={"description"}
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. | " +
+            "Купить и продать недвижимость в любое время в Южной Корее. | " +
+            "한국 어디서나 부동산을 사고팔 수 있습니다. | "
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
